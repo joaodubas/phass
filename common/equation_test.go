010c2c33f8678cd65c1e62c62dc7e898d85703be
Joao Paulo Dubas <joao.dubas@gmail.com>
2015-05-23 16:52:41 -0300

    [update] Move validators to common pack.

--- common/equation_test.go
+++ common/equation_test.go
@@ -9,23 +9,23 @@ import (
 
 func TestEquationValidation(t *testing.T) {
 	cases := []case_{
-		case_{in: map[string]float64{}, rsVal: false, errVal: "Missing measure"},
-		case_{in: map[string]float64{"age": 18.0}, rsVal: false, errVal: "Missing measure"},
-		case_{in: map[string]float64{"sskf": 112.1}, rsVal: false, errVal: "Missing measure"},
-		case_{in: map[string]float64{"age": 17.9, "sskf": 112.1}, rsVal: false, errVal: "Equation valid for age"},
+		case_{in: map[string]float64{}, ok: false, err: "Missing measure"},
+		case_{in: map[string]float64{"age": 18.0}, ok: false, err: "Missing measure"},
+		case_{in: map[string]float64{"sskf": 112.1}, ok: false, err: "Missing measure"},
+		case_{in: map[string]float64{"age": 17.9, "sskf": 112.1}, ok: false, err: "Equation valid for age"},
 	}
 
 	for _, data := range cases {
 		eq := NewEquation(data.in, conf)
-		if v, err := eq.Validator(); v != data.rsVal {
+		if v, err := eq.Validator(); v != data.ok {
 			t.Error("Should get an error.")
-		} else if !strings.Contains(err.Error(), data.errVal) {
+		} else if !strings.Contains(err.Error(), data.err) {
 			t.Error("Should get proper error message.")
 		}
 
 		if v, err := eq.Calc(); v > 0.0 {
 			t.Error("Value should be 0")
-		} else if !strings.Contains(err.Error(), data.errVal) {
+		} else if !strings.Contains(err.Error(), data.err) {
 			t.Error("Should get proper error message.")
 		}
 	}
@@ -63,10 +63,60 @@ func TestEquationRetrieveIn(t *testing.T) {
 	}
 }
 
+func TestAgeValidator(t *testing.T) {
+	cases := []case_{
+		case_{
+			in: map[string]float64{},
+			ok: false,
+			err: "Missing age",
+		},
+		case_{
+			in: map[string]float64{"age": 9},
+			ok: false,
+			err: "Valid for ages",
+		},
+		case_{
+			in: map[string]float64{"age": 21},
+			ok: false,
+			err: "Valid for ages",
+		},
+	}
+
+	validator := ValidateAge(10, 20)
+	for _, data := range cases {
+		eq := NewEquation(data.in, conf).(*Equation)
+		if ok, err := validator(eq); ok != data.ok {
+			t.Error("Should receive a proper boolean")
+		} else if !strings.Contains(err.Error(), data.err) {
+			t.Error("Should show proper error message")
+		}
+	}
+
+	eq := NewEquation(map[string]float64{"age": 15}, conf).(*Equation)
+	if ok, err := validator(eq); !ok {
+		t.Error("Should receive proper boolean")
+	} else if err != nil {
+		t.Error("Should not get an error")
+	}
+}
+
+func TestMeasureValidator(t *testing.T) {
+	cases := []case_{}
+	validator := ValidateMeasures([]string{"age", "weight", "height"})
+	for _, data := range cases {
+		eq := NewEquation(data.in, conf).(*Equation)
+		if ok, err := validator(eq); ok != data.ok {
+			t.Error("Should receive proper boolean")
+		} else if strings.Contains(err.Error(), data.err) {
+			t.Error("Should show proper error message.")
+		}
+	}
+}
+
 type case_ struct {
-	in     map[string]float64
-	rsVal  bool
-	errVal string
+	in     InParams
+	ok bool
+	err string
 }
 
 var conf = NewEquationConf(
