2479d0e485ffa78dab9a4af1a6f8824e8d23692b
Joao Paulo Dubas <joao.dubas@gmail.com>
2015-05-23 16:55:52 -0300

    [update] Equation conf definition in body fat.

--- bodyfat/equation_female.go
+++ bodyfat/equation_female.go
@@ -19,20 +19,16 @@ var womenSevenSSKF = []int{
 
 var confWomenSevenSKF = common.NewEquationConf(
 	"Seven skinfold equation from Pollock",
-	[]func(*common.Equation) (bool, error){
+	[]common.Validate{
 		common.ValidateMeasures([]string{"gender", "age", "sskf"}),
 		common.ValidateGender(assess.Female),
 		common.ValidateAge(18, 55),
 	},
-	func(e *common.Equation) (float64, error) {
-		if ok, err := e.Validator(); !ok {
-			return 0.0, err
-		}
-
+	func(e *common.Equation) float64 {
 		age, _ := e.In("age")
 		sskf, _ := e.In("sskf")
 		d := 1.097 - 0.00046971*sskf + 0.00000056*math.Pow(sskf, 2) - 0.00012828*age
 
-		return (5.01/d - 4.57) * 100.0, nil
+		return (5.01/d - 4.57) * 100.0
 	},
 )
--- bodyfat/equation_male.go
+++ bodyfat/equation_male.go
@@ -19,20 +19,16 @@ var menSevenSSKF = []int{
 
 var confMenSevenSKF = common.NewEquationConf(
 	"Seven skinfold equation from Pollock",
-	[]func(*common.Equation) (bool, error){
+	[]common.Validate{
 		common.ValidateMeasures([]string{"gender", "age", "sskf"}),
 		common.ValidateGender(assess.Male),
 		common.ValidateAge(18, 61),
 	},
-	func(e *common.Equation) (float64, error) {
-		if ok, err := e.Validator(); !ok {
-			return 0.0, err
-		}
-
+	func(e *common.Equation) float64 {
 		age, _ := e.In("age")
 		sskf, _ := e.In("sskf")
 		d := 1.112 - 0.00043499*sskf + 0.00000055*math.Pow(sskf, 2) - 0.0002882*age
 
-		return (4.95/d - 4.5) * 100.0, nil
+		return (4.95/d - 4.5) * 100.0
 	},
 )
