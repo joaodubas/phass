ff732d90676b4bd6dd94f3b8857a7b9bdf82ef9e
Joao Paulo Dubas <joao.dubas@gmail.com>
2015-05-23 22:02:00 -0300

    [update] body comp equations implement new conf.

--- bodyfat/equation_female.go
+++ bodyfat/equation_female.go
@@ -7,6 +7,25 @@ import (
 	"math"
 )
 
+func NewWomenSevenSKF(p *assess.Person, a *assess.Assessment, s *skf.Skinfolds) *BodyCompositionSKF {
+	return NewBodyCompositionSKF(p, a, s, confWomenSevenSKF)
+}
+
+var confWomenSevenSKF = NewEquationConfForSKF(SKFEquationConf{
+	name:      "Seven skinfold equation from Pollock",
+	gender:    assess.Female,
+	lowerAge:  18,
+	upperAge:  55,
+	skinfolds: womenSevenSSKF,
+	equation: func(e *common.Equation) float64 {
+		age, _ := e.In("age")
+		sskf, _ := e.In("sskf")
+		d := 1.097 - 0.00046971*sskf + 0.00000056*math.Pow(sskf, 2) - 0.00012828*age
+
+		return (5.01/d - 4.57) * 100.0
+	},
+})
+
 var womenSevenSSKF = []int{
 	skf.SKFChest,
 	skf.SKFAbdominal,
@@ -16,22 +35,3 @@ var womenSevenSSKF = []int{
 	skf.SKFTriceps,
 	skf.SKFMidaxillary,
 }
-
-var confWomenSevenSKF = common.NewEquationConf(
-	"Seven skinfold equation from Pollock",
-	func(i interface{}) common.InParams {
-		return map[string]float64{}
-	},
-	[]common.Validator{
-		common.ValidateMeasures([]string{"gender", "age", "sskf"}),
-		common.ValidateGender(assess.Female),
-		common.ValidateAge(18, 55),
-	},
-	func(e *common.Equation) float64 {
-		age, _ := e.In("age")
-		sskf, _ := e.In("sskf")
-		d := 1.097 - 0.00046971*sskf + 0.00000056*math.Pow(sskf, 2) - 0.00012828*age
-
-		return (5.01/d - 4.57) * 100.0
-	},
-)
--- bodyfat/equation_male.go
+++ bodyfat/equation_male.go
@@ -7,6 +7,25 @@ import (
 	"math"
 )
 
+func NewMenSevenSKF(p *assess.Person, a *assess.Assessment, s *skf.Skinfolds) *BodyCompositionSKF {
+	return NewBodyCompositionSKF(p, a, s, confMenSevenSKF)
+}
+
+var confMenSevenSKF = NewEquationConfForSKF(SKFEquationConf{
+	name:      "Seven skinfold equation from Pollock",
+	gender:    assess.Male,
+	lowerAge:  18,
+	upperAge:  61,
+	skinfolds: menSevenSSKF,
+	equation: func(e *common.Equation) float64 {
+		age, _ := e.In("age")
+		sskf, _ := e.In("sskf")
+		d := 1.112 - 0.00043499*sskf + 0.00000055*math.Pow(sskf, 2) - 0.0002882*age
+
+		return (4.95/d - 4.5) * 100.0
+	},
+})
+
 var menSevenSSKF = []int{
 	skf.SKFChest,
 	skf.SKFAbdominal,
@@ -16,22 +35,3 @@ var menSevenSSKF = []int{
 	skf.SKFTriceps,
 	skf.SKFMidaxillary,
 }
-
-var confMenSevenSKF = common.NewEquationConf(
-	"Seven skinfold equation from Pollock",
-	func(i interface{}) common.InParams {
-		return map[string]float64{}
-	},
-	[]common.Validator{
-		common.ValidateMeasures([]string{"gender", "age", "sskf"}),
-		common.ValidateGender(assess.Male),
-		common.ValidateAge(18, 61),
-	},
-	func(e *common.Equation) float64 {
-		age, _ := e.In("age")
-		sskf, _ := e.In("sskf")
-		d := 1.112 - 0.00043499*sskf + 0.00000055*math.Pow(sskf, 2) - 0.0002882*age
-
-		return (4.95/d - 4.5) * 100.0
-	},
-)
