397526ca00dc2df821d5a3fa45d6a242ed59aafe
Joao Paulo Dubas <joao.dubas@gmail.com>
2015-05-23 23:18:53 -0300

    [update] show body fat.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	anthropo "github.com/joaodubas/phass/anthropometry"
 	assess "github.com/joaodubas/phass/assessment"
+	bf "github.com/joaodubas/phass/bodyfat"
 	skf "github.com/joaodubas/phass/skinfold"
 	"os"
 )
@@ -16,6 +17,10 @@ func main() {
 	handleError("Ops person was not born:", err)
 	fmt.Printf("Age in years: %.2f\nAge in months %.2f\n", p.Age(), p.AgeInMonths())
 
+	a, err := assess.NewAssessment("2015-Dec-15")
+	handleError("Ops assessment not done:", err)
+	fmt.Println(a)
+
 	skfs := skf.NewSkinfolds(map[int]float64{
 		skf.SKFChest:       5.0,
 		skf.SKFAbdominal:   10.0,
@@ -26,6 +31,11 @@ func main() {
 		skf.SKFSuprailiac:  35.0,
 	})
 	fmt.Println(skfs)
+
+	comp := bf.NewWomenSevenSKF(p, a, skfs)
+	fmt.Println(comp)
+	comp = bf.NewMenSevenSKF(p, a, skfs)
+	fmt.Println(comp)
 }
 
 func handleError(tmpl string, err error) {
