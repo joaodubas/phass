bd34e2963b90431e48447373b0fcdfdf6c448380
Joao Paulo Dubas <joao.dubas@gmail.com>
2015-05-25 14:02:59 -0300

    [add] Some more body fat equations.

--- bodyfat/equation_female.go
+++ bodyfat/equation_female.go
@@ -7,27 +7,63 @@ import (
 	"math"
 )
 
-var NewWomenSevenSKF = FactoryBodyCompositionSKF(confWomenSevenSKF)
+var (
+	NewWomenSevenSKF = FactoryBodyCompositionSKF(confWomenSevenSKF)
+	NewWomenThreeSKF = FactoryBodyCompositionSKF(confWomenThreeSKF)
+	NewWomenTwoSKF   = FactoryBodyCompositionSKF(confWomenTwoSKF)
+)
 
 var confWomenSevenSKF = SKFEquationConf{
-	name:     "Seven skinfold equation from Pollock",
+	name:     "Women seven skinfold equation from Jackson, Pollock, Ward",
 	gender:   assess.Female,
 	lowerAge: 18,
 	upperAge: 55,
 	skinfolds: []int{
+		skf.SKFSubscapular,
+		skf.SKFTriceps,
 		skf.SKFChest,
+		skf.SKFMidaxillary,
+		skf.SKFSuprailiac,
 		skf.SKFAbdominal,
 		skf.SKFThigh,
-		skf.SKFAbdominal,
-		skf.SKFSuprailiac,
-		skf.SKFTriceps,
-		skf.SKFMidaxillary,
 	},
 	equation: func(e *common.Equation) float64 {
 		age, _ := e.In("age")
 		sskf, _ := e.In("sskf")
 		d := 1.097 - 0.00046971*sskf + 0.00000056*math.Pow(sskf, 2) - 0.00012828*age
-
 		return (5.01/d - 4.57) * 100.0
+	},
+}
+
+var confWomenThreeSKF = SKFEquationConf{
+	name:     "Women three skinfold equation from Jackson, Pollock, Ward",
+	gender:   assess.Female,
+	lowerAge: 18,
+	upperAge: 55,
+	skinfolds: []int{
+		skf.SKFTriceps,
+		skf.SKFSuprailiac,
+		skf.SKFThigh,
+	},
+	equation: func(e *common.Equation) float64 {
+		age, _ := e.In("age")
+		sskf, _ := e.In("sskf")
+		d := 1.0994921 - 0.0009929*sskf + 0.0000023*math.Pow(sskf, 2) - 0.0001392*age
+		return (5.01/d - 4.57) * 100
+	},
+}
+
+var confWomenTwoSKF = SKFEquationConf{
+	name:     "Women two skinfold equation from Slaughter et al.",
+	gender:   assess.Female,
+	lowerAge: 6,
+	upperAge: 17,
+	skinfolds: []int{
+		skf.SKFTriceps,
+		skf.SKFCalf,
+	},
+	equation: func(e *common.Equation) float64 {
+		sskf, _ := e.In("sskf")
+		return 0.735*sskf + 1.0
 	},
 }
--- bodyfat/equation_male.go
+++ bodyfat/equation_male.go
@@ -7,10 +7,36 @@ import (
 	"math"
 )
 
-var NewMenSevenSKF = FactoryBodyCompositionSKF(confMenSevenSKF)
+var (
+	NewMenSevenSKF = FactoryBodyCompositionSKF(confMenSevenSKF)
+	NewMenThreeSKF = FactoryBodyCompositionSKF(confMenThreeSKF)
+	NewMentTwoSKF  = FactoryBodyCompositionSKF(confMenTwoSKF)
+)
 
 var confMenSevenSKF = SKFEquationConf{
-	name:     "Seven skinfold equation from Pollock",
+	name:     "Men seven skinfold equation from Jackson, Pollock",
+	gender:   assess.Male,
+	lowerAge: 18,
+	upperAge: 61,
+	skinfolds: []int{
+		skf.SKFSubscapular,
+		skf.SKFTriceps,
+		skf.SKFChest,
+		skf.SKFMidaxillary,
+		skf.SKFSuprailiac,
+		skf.SKFAbdominal,
+		skf.SKFThigh,
+	},
+	equation: func(e *common.Equation) float64 {
+		age, _ := e.In("age")
+		sskf, _ := e.In("sskf")
+		d := 1.112 - 0.00043499*sskf + 0.00000055*math.Pow(sskf, 2) - 0.0002882*age
+		return (4.95/d - 4.5) * 100.0
+	},
+}
+
+var confMenThreeSKF = SKFEquationConf{
+	name:     "Men three skinfold equation from Jackson, Pollock",
 	gender:   assess.Male,
 	lowerAge: 18,
 	upperAge: 61,
@@ -18,16 +44,26 @@ var confMenSevenSKF = SKFEquationConf{
 		skf.SKFChest,
 		skf.SKFAbdominal,
 		skf.SKFThigh,
-		skf.SKFAbdominal,
-		skf.SKFSuprailiac,
-		skf.SKFTriceps,
-		skf.SKFMidaxillary,
 	},
 	equation: func(e *common.Equation) float64 {
 		age, _ := e.In("age")
 		sskf, _ := e.In("sskf")
-		d := 1.112 - 0.00043499*sskf + 0.00000055*math.Pow(sskf, 2) - 0.0002882*age
-
+		d := 1.109380 - 0.0008267*sskf + 0.0000016*math.Pow(sskf, 2) - 0.0002574*age
 		return (4.95/d - 4.5) * 100.0
+	},
+}
+
+var confMenTwoSKF = SKFEquationConf{
+	name:     "Men two skinfold equation from Slaughter et al.",
+	gender:   assess.Male,
+	lowerAge: 6,
+	upperAge: 17,
+	skinfolds: []int{
+		skf.SKFTriceps,
+		skf.SKFCalf,
+	},
+	equation: func(e *common.Equation) float64 {
+		sskf, _ := e.In("sskf")
+		return 0.610*sskf + 5.1
 	},
 }
