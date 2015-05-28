6ff9a6d408e2202d066ed7050099d65f281ae0bf
Joao Paulo Dubas <joao.dubas@gmail.com>
2015-05-28 11:57:47 -0300

    [update] move common to root

--- common/common.go
+++ common/common.go
@@ -1,5 +1,115 @@
 package common
 
+import "fmt"
+
+/**
+ * Equation
+ */
+
+type Equation struct {
+	in   map[string]float64
+	conf *EquationConf
+}
+
+func NewEquation(in InParams, conf *EquationConf) Equationer {
+	return &Equation{in: in, conf: conf}
+}
+
+func (e *Equation) String() string {
+	return e.conf.Name
+}
+
+func (e *Equation) In(k string) (float64, bool) {
+	v, ok := e.in[k]
+	return v, ok
+}
+
+func (e *Equation) Validate() (bool, error) {
+	for _, f := range e.conf.Validators {
+		if r, err := f(e); err != nil {
+			return r, err
+		}
+	}
+	return true, nil
+}
+
+func (e *Equation) Calc() (float64, error) {
+	if r, e := e.Validate(); !r {
+		return 0.0, e
+	}
+	return e.conf.Calc(e), nil
+}
+
+type EquationConf struct {
+	Name       string
+	Extract    Extractor
+	Validators []Validator
+	Calc       Calculator
+}
+
+func NewEquationConf(name string, e Extractor, v []Validator, eq Calculator) *EquationConf {
+	return &EquationConf{
+		Name:       name,
+		Extract:    e,
+		Validators: v,
+		Calc:       eq,
+	}
+}
+
+type (
+	InParams   map[string]float64
+	Extractor  func(interface{}) InParams
+	Validator  func(*Equation) (bool, error)
+	Calculator func(*Equation) float64
+)
+
+type Equationer interface {
+	In(string) (float64, bool)
+	Validate() (bool, error)
+	Calc() (float64, error)
+}
+
+/**
+ * Validation
+ */
+
+func ValidateGender(expect int) Validator {
+	return func(e *Equation) (bool, error) {
+		if g, ok := e.In("gender"); !ok {
+			return false, fmt.Errorf("Missing gender")
+		} else if int(g) != expect {
+			return false, fmt.Errorf("Valid for gender %d", expect)
+		}
+		return true, nil
+	}
+}
+
+func ValidateAge(lower, upper float64) Validator {
+	return func(e *Equation) (bool, error) {
+		if age, ok := e.In("age"); !ok {
+			return false, fmt.Errorf("Missing age measure")
+		} else if age < lower || age > upper {
+			return false, fmt.Errorf("Valid for ages between %.0f and %.0f", lower, upper)
+		}
+		return true, nil
+	}
+}
+
+func ValidateMeasures(expect []string) Validator {
+	return func(e *Equation) (bool, error) {
+		for _, k := range expect {
+			if _, ok := e.In(k); !ok {
+				return false, fmt.Errorf("Missing %s measure", k)
+			}
+		}
+		return true, nil
+	}
+}
+
+/**
+ * Classification
+ */
+
 func Classifier(value float64, classes map[int][2]float64, mapper map[int]string) string {
 	cid := classifierId(value, classes)
 	class, ok := mapper[cid]
